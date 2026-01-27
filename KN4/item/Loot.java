3af3ecdf652533e77cb8a32487021abc0fb2c7a5
unknown <beda.brunner@mgb.ch>
2026-01-27 09:54:15 +0100

    Add player infentory

--- KN4/item/Loot.java
+++ KN4/item/Loot.java
@@ -2,7 +2,7 @@ package item;
 
 public class Loot {
   private String explainloot; // location bag or chest like something
-  private int money; // loot its self like money or something
+  private int money; // lootwoth its self like money or something
 
   public Loot(String s, int i) {
     this.explainloot = s;
--- KN4/player/Player.java
+++ KN4/player/Player.java
@@ -1,16 +1,29 @@
 package player;
 
+import item.Loot;
+
+import java.util.ArrayList;
+
 public class Player {
   private String name;
   private int passivHealSkill;
   private int health;
   private int money;
+  private ArrayList<Loot> inventory;
 
   public Player(String name, int healthi){
     this.name = name;
     this.passivHealSkill = 10;
     this.health = healthi;
     this.money = 5;
+  }
+
+  public ArrayList<Loot> getInventory() {
+    return inventory;
+  }
+
+  public void setInventory(ArrayList<Loot> inventory) {
+      this.inventory = inventory;
   }
 
   public int getMoney() {
